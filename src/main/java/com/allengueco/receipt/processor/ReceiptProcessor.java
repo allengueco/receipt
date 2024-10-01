608dc5a0b7f00bb71bc1bbb47e54837fe21b92b9
Allen Gueco <allen.gueco1@gmail.com>
2024-09-30 20:19:20 -0400

    base processor

---
+++ src/main/java/com/allengueco/receipt/processor/ReceiptProcessor.java
@@ -0,0 +1,32 @@
+package com.allengueco.receipt.processor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.allengueco.receipt.model.Receipt;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE) // so we use this one first, which kicks off the rest of the pipeline
+public class ReceiptProcessor implements AbstractProcessor {
+    private final Logger log = LoggerFactory.getLogger(ReceiptProcessor.class);
+    List<AbstractProcessor> processors;
+
+    public ReceiptProcessor(List<AbstractProcessor> processors) {
+        this.processors = processors;
+    }
+
+    @Override
+    public int process(Receipt receipt) {
+        log.info("Starting process for receiptId: {}", receipt.getId());
+
+        return processors.stream()
+            .collect(Collectors.summingInt(p -> p.process(receipt)));
+    }
+
+}
